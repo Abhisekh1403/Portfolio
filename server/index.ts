20d5565da82fc4522e84b075f169e87f35d54a80
Sunny619 <abhigyan260902@gmail.com>
2024-03-30 21:45:36 +0530

    Fixed issues

--- server/index.ts
+++ server/index.ts
@@ -39,8 +39,8 @@ app.post('/api/send-email', (req, res) => {
         .then(() => {
             transporter
                 .sendMail({
-                    from: `"${name}" <henryheffernan.folio@gmail.com>`, // sender address
-                    to: 'henryheffernan@gmail.com, henryheffernan.folio@gmail.com', // list of receivers
+                    from: `"${name}" <abhigyan260902@gmail.com>`, // sender address
+                    to: 'abhigyan260902@gmail.com, abhigyan260902@gmail.com', // list of receivers
                     subject: `${name} <${email}> ${
                         company ? `from ${company}` : ''
                     } submitted a contact form`, // Subject line
