151270d9c2d200cbe9731421148278cb09b0bd43
Sunny619 <abhigyan260902@gmail.com>
2024-04-09 14:01:08 +0530

    Changed inner endpoint

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'https://abhigyan-ubuntu.onrender.com/';
+        iframe.src = 'https://portfolio-inner-mpfbzvap5a-uc.a.run.app/';
         /**
          * Use dev server is query params are present
          *
